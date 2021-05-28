04c8c6f15965d88f83d3ef78682a02eff4b7343c
flino1934 <f.lino1934@hotmail.com>
2021-05-27 23:24:46 -0300

    Criando projeto hr-payroll

---
+++ hr-payroll/src/test/java/com/lino/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lino.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

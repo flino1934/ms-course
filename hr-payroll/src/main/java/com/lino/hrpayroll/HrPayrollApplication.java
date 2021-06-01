818b36bd9b2cce8cd5206b4398ea8ef4b92e62cd
flino1934 <f.lino1934@hotmail.com>
2021-05-31 23:47:10 -0300

    Configuação do eureka para subir os microserviços

--- hr-payroll/src/main/java/com/lino/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/lino/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,11 @@ package com.lino.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@EnableFeignClients//vai conectar um projeto com o outro usando o feing ao invés de RestTemplate
-@RibbonClient(name = "hr-worker")//vamos configurar a aplicação como um cliente ribbon que vai fazer o balanceamento
+@EnableEurekaClient
+@EnableFeignClients//vamos configurar a aplicação como um cliente FeignClients que vai fazer o balanceamento
 @SpringBootApplication
 public class HrPayrollApplication {
 

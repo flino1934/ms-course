1a07e6e1add10468d8efb5f9524b40498099575a
flino1934 <f.lino1934@hotmail.com>
2021-06-02 00:22:42 -0300

    Congiguration zull api gateway

--- hr-api-gateway-zuul/src/main/java/com/lino/hrapigateawayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/lino/hrapigateawayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.lino.hrapigateawayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy//o Zull tambem se conecta com o eureka
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 

58a4e9a14bee605c86b9fdd1ea9d0ef92442e49c
flino1934 <f.lino1934@hotmail.com>
2021-06-03 18:00:33 -0300

    Configurations hr-config-server

---
+++ hr-config-serve/src/main/java/com/lino/hrconfigserve/HrConfigServeApplication.java
@@ -0,0 +1,13 @@
+package com.lino.hrconfigserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServeApplication.class, args);
+	}
+
+}
---
+++ hr-config-serve/src/test/java/com/lino/hrconfigserve/HrConfigServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lino.hrconfigserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
